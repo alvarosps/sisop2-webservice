63b8ed2e462fb145c6fdf9244bfde7f19d44c83a
ALvaro SIlva <alvaro123sps@gmail.com>
2021-04-11 21:44:50 -0300

    Created client

--- webservice/CalculadoraRest.java
+++ webservice/CalculadoraRest.java
@@ -6,24 +6,20 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("/calculadora")
+@Path("calculadora")
 public class CalculadoraRest {   
 
-	@GET
 	@Path("/somarInt/{a}/{b})")
 	@Produces(MediaType.APPLICATION_JSON)
-	public int somarInt(@PathParam("a") int a, @PathParam("b") int b) {
-		Calculadora c = new Calculadora(a, b, "+");
-
-		return c.getResultado();
+	@GET
+	public Calculadora somarInt(@PathParam("a") int a, @PathParam("b") int b) {
+		return new Calculadora(a, b, "+");
 	}
 
-	@GET
 	@Path("/multiplicarInt/{a}/{b})")
 	@Produces(MediaType.APPLICATION_JSON)
-	public int multiplicarInt(@PathParam("a") int a, @PathParam("b") int b) {
-		Calculadora c = new Calculadora(a, b, "*");
-
-		return c.getResultado();
+	@GET
+	public Calculadora multiplicarInt(@PathParam("a") int a, @PathParam("b") int b) {
+		return new Calculadora(a, b, "*");
 	}
 }
